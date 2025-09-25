535e6a985ad012b5f17cb579bfc629af77f10075
Abhijeet Gopalakrishnan <abhijeetgopalakrishnan@gmail.com>
2025-09-25 16:01:12 -0400

    Update

--- run_ice.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#SBATCH -J TML
-#SBATCH --time=4:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
-#SBATCH --output tml.output
-
-srun ~/bin/exaflow3D incompressible_tml.ctr > log
--- run_phoenix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -JTML512
-#SBATCH -A gts-ssuresh313-startup
-#SBATCH -t5:00:00
-#SBATCH -n256 
-#SBATCH -otml.output
-
-srun ~/abhijeet/bin/exaflow3D incompressible_tml.ctr > log
