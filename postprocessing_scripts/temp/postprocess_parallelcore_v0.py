ff51f7c96942d77de072ef96a6aeeaac07349b8c
Abhijeet Gopalakrishnan <abhijeetgopalakrishnan@gmail.com>
2025-09-26 13:49:22 -0400

    Update

--- postprocessing_scripts/temp/postprocess_parallelcore_v0.py
+++ postprocessing_scripts/temp/postprocess_parallelcore_v0.py
@@ -1,9 +1,10 @@
-from concurrent.futures import ProcesspoolExecutor as pool
+from concurrent.futures import ProcessPoolExecutor as ex
 from FPCSLpy.case import Case
 import numpy as np
 import os
 import csv
 import argparse
+
 
 def parse_args():
     """ Parse arguments from command line
@@ -68,18 +69,65 @@ def plot_thickness(time_steps, delta, x_label, y_label, file_name):
     plt.savefig(file_name)
     plt.close()
 
-def momentum_thickness(U, u, alpha):
-    """ Compute momentum thickness at each timestep
+
+def momentum_thickness(U, u_bar, delta_u, alpha, dy):
+    """ Compute momentum thickness at each timestep per \\cite{lee2025effects}.
     
     Args:
-        U (float): Gas(or phase1) free stream velocity
-        u (float):
+        U       (float) : Gas(or phase1) free stream velocity
+        u_bar   (float) : Reynolds averaged 
+        delta_u (float) : Difference in free stream velocities
+        alpha   (float) : Volume fraction
+        dy      (float) : Cell(?) size along y direction
 
-    integrand_m = ((alpha) * (U_g - u1_t) * (u1_t))/(U_g * U_g)
-    delta_theta_g.append(np.trapz(integrand_m, dx=dy))
+    Return:
+        delta_theta (float) : Momentum thickness for a given time-step
+    
+    """
+
+    integrand     = ((alpha) * (U_g - u_bar) * (u_bar))/(delta_u * delta_u)
+    delta_theta_g = (np.trapz(integrand, dx=dy))
+    
+    return delta_theta_g
 
 
-def main():
+def phi_thickness(alpha, nx_g_half):
+    """ Computing vol fraction thickness for insight into entrainement of one
+        phase into another.
+    
+    Args:
+        alpha     (float)   : Volume fraction
+        nx_g_half (int)     : Half the grid size of the given simulation
+    
+    Return:
+        delta_phi_g (float) : Volume fraction based thickness
+    """
+
+    integrand_phi           = alpha
+    integrand_phi_subset    = integrand_phi[:nx_g_half]
+    delta_phi_g             = np.trapz(integrand_phi_subset, dx=dy)
+    
+    return delta_phi_g
+
+
+def mixinglayer_thickness(alpha, dy)
+    """ Computing the mixing layer thickness
+    
+    Args:
+        alpha (float) : Volume fraction
+        dy    (float) : Cell(?) size along y direction
+
+    Return:
+        delta_mixing (float) : Mixing layer thickness
+    """
+    
+    integrand_mixing = alpha * (1 - alpha)
+    delta_mixing     = np.trapz(integrand_mixing, dx=dy)
+    
+    return delta_mixing
+    
+
+def case_update():
     case = Case(path='./.')
 
     args = parse_args()
@@ -98,9 +146,20 @@ def main():
     to_update_parameters['simulation_parameters']['parallel']['nzsd']           = 32
     to_update_parameters['simulation_parameters']['solvers']['incompressible']  = True
 
+    dx, dy, dz = case.grid['dx'], case.grid['dy'], case.grid['dz']
+
     #Update and check parameters
     case.update_parameters(to_update_parameters)
+
+    return  nx_g, ny_g, nz_g \
+            dx, dy, dz
+
+
+def main():
     
+if __name__ == "__main__":
+
+    #Variables
     delta_theta_g   = []
     delta_phi_g     = []
     delta_mixing    = []
@@ -108,8 +167,5 @@ def main():
     delta           = (2. * np.pi) / 100.
     U_g             = 3.1830988618379066
     dt              = 0.00025
-    dx, dy, dz      = case.grid['dx'], case.grid['dy'], case.grid['dz']
 
-if __name__ == "__main__":
     main()
-    print("Hello world") 
