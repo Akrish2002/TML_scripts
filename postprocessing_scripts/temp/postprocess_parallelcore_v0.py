f03fc38f4fe7487e72d410a82cbaf7d6caef04af
Abhijeet Gopalakrishnan <abhijeetgopalakrishnan@gmail.com>
2025-09-27 00:18:18 -0400

    Update

--- postprocessing_scripts/temp/postprocess_parallelcore_v0.py
+++ postprocessing_scripts/temp/postprocess_parallelcore_v0.py
@@ -85,13 +85,13 @@ def momentum_thickness(U, u_bar, delta_u, alpha, dy):
     
     """
 
-    integrand     = ((alpha) * (U_g - u_bar) * (u_bar))/(delta_u * delta_u)
+    integrand     = ((alpha) * (U - u_bar) * (u_bar))/(delta_u * delta_u)
     delta_theta_g = (np.trapz(integrand, dx=dy))
     
     return delta_theta_g
 
 
-def phi_thickness(alpha, nx_g_half):
+def phi_thickness(alpha, nx_g_half, dy):
     """ Computing vol fraction thickness for insight into entrainement of one
         phase into another.
     
@@ -136,7 +136,8 @@ def case_update():
     
     """
 
-    case = Case(path='./.')
+    case        = Case(path='./.')
+    update_file = './incompressible.ctr'
 
     args = parse_args()
     nx_g, ny_g, nz_g = args.nx_g, args.ny_g, args.nz_g
@@ -145,27 +146,27 @@ def case_update():
     to_update_parameters = case.parameters
     to_update_parameters['grid']['x_max']                                       = 2. * np.pi
     to_update_parameters['grid']['y_max']                                       = 2. * np.pi
-    to_update_parameters['grid']['z_max']                                       = 2. * np.pi
+    to_update_parameters['grid']['z_max']                                       = 5 * (2.*np.pi/nx_g)
     to_update_parameters['grid']['nx']                                          = nx_g
     to_update_parameters['grid']['ny']                                          = ny_g
     to_update_parameters['grid']['nz']                                          = nz_g
     to_update_parameters['simulation_parameters']['parallel']['nxsd']           = 32
     to_update_parameters['simulation_parameters']['parallel']['nysd']           = 32
-    to_update_parameters['simulation_parameters']['parallel']['nzsd']           = 32
+    to_update_parameters['simulation_parameters']['parallel']['nzsd']           = 1
     to_update_parameters['simulation_parameters']['solvers']['incompressible']  = True
-
-    dx, dy, dz = case.grid['dx'], case.grid['dy'], case.grid['dz']
 
     #Update and check parameters
     case.update_parameters(to_update_parameters)
 
-    return  nx_g, ny_g, nz_g \
-            dx, dy, dz       \
+    dx, dy, dz = case.grid['dx'], case.grid['dy'], case.grid['dz']
+
+    return  nx_g, ny_g, nz_g,   \
+            dx, dy, dz,         \
             case
 
 
 def call_cores( delta_theta_g, delta_phi_g, delta_mixing,
-                delta, U_g, dt,
+                delta, U_g, dt, delta_u,
                 
                 nx_g, ny_g, nz_g,
                 nx_g_half,
@@ -181,15 +182,17 @@ def call_cores( delta_theta_g, delta_phi_g, delta_mixing,
     
     """
 
+    case.read_time_steps([time_step], to_interpolate=True)
     u, v, w  = case.data[f'{time_step}']['u'], \
                case.data[f'{time_step}']['v'], \
                case.data[f'{time_step}']['w']
+
     u_bar = np.mean(u, axis=(0, 2))                                             #Avg along x and z since it is periodic
-    alpha = np.mean(1 - (case.data[f"{time_steps}"]["phi_2"]), axis=(0, 2))     #1 - phi_2
+    alpha = np.mean(1 - (case.data[f"{time_step}"]["phi_2"]), axis=(0, 2))      #1 - phi_2
     
-    delta_theta_g   = momentum_thickness(U_g, u_bar, delta_u, alpha, dy)
-    delta_phi_g     = phi_thickness(alpha, nx_g_half)
-    delta_mixing    = mixinglayer_thickness(alpha, dy)
+    return  momentum_thickness(U_g, u_bar, delta_u, alpha, dy), \
+            phi_thickness(alpha, nx_g_half, dy), \
+            mixinglayer_thickness(alpha, dy)
 
 
 def main():
@@ -208,30 +211,36 @@ def main():
 
     delta           = (2. * np.pi) / 100.
     U_g             = 3.1830988618379066
+    U_l             = 0.
+    delta_u         = U_g - U_l
     dt              = 0.00025
-    cores           = 50
+    cores           = 24
 
-    (nx_g, ny_g, nz_g
-     dx, dy, dz       
+    (nx_g, ny_g, nz_g,
+     dx, dy, dz,       
      case)             = case_update()
+    nx_g_half          = int(nx_g / 2)
 
     #For time-steps_{i}
-    time_steps = [i for i in range(0, 98000, 2000)]
+    time_steps = [i for i in range(0, 31200, 800)]
 
     with ProcessPoolExecutor(max_workers=cores) as ex: 
-        ex.submit( call_cores, 
+         futures  =                 [ex.submit(call_cores, 
 
-                   delta_theta_g, delta_phi_g, delta_mixing,
-                   delta, U_g, dt,
-                   
-                   nx_g, ny_g, nz_g,
-                   nx_g_half,
-                   dx, dy, dz,
-                   case,
-                   
-                   time_step) for time_step in time_steps
+                                    delta_theta_g, delta_phi_g, delta_mixing,
+                                    delta, U_g, dt, delta_u,
+                                    
+                                    nx_g, ny_g, nz_g,
+                                    nx_g_half,
+                                    dx, dy, dz,
+                                    case,
+                                    
+                                    time_step) for time_step in time_steps ]
  
- 
+    return futures
+
+
 if __name__ == "__main__":
-
-
+    futures = main()
+    results = np.array([f.result() for f in futures])
+    print(results)
