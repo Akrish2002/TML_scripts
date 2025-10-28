6a19aec1b9012e13616d6557a2661310272da198
Abhijeet Gopalakrishnan <abhi@login03.frontier.olcf.ornl.gov>
2025-10-28 17:56:24 -0400

    Quick folder creation bash script

---
+++ bash_scripts/create_folders.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+TMLSCRIPTS_INITIALIZATION="/ccs/home/abhi/member-work/incompressible-tml/TML_scripts/initialization_scripts"
+TMLSCRIPTS_BATCH="/ccs/home/abhi/member-work/incompressible-tml/TML_scripts/batch_scripts"
+
+#Take in number of folders to create
+echo -n "--Number of folders? "
+read -r nfolders
+echo -n "--Enter folder location: "
+read -r PATH_TO_CREATED_FOLDERS
+
+#Take arg for folder names
+for i in $(seq 1 $nfolders)
+do
+    if [[ $i == 1 ]]; then
+        echo -n "--Enter folder name: "
+    else
+        echo -n "--Next folder: "
+    fi
+    read -r fname 
+    mkdir $fname
+
+    #Making subfolders in each
+    cd $fname
+    mkdir "log"
+    mkdir "plots"
+
+    #Copying the skeleton .ctr and .sl files from TML_scripts
+    #cp $TMLSCRIPTS_INITIALIZATION/
+    cp $TMLSCRIPTS_BATCH/batch.sl                   $PATH_TO_CREATED_FOLDERS
+    cp $TMLSCRIPTS_BATCH/incompressible_tml.ctr     $PATH_TO_CREATED_FOLDERS
+
+    #Leave folder
+    cd ..
+done
+
