82b10183e4f832452b0b1279a7cd1f571c9877bf
Abhijeet Gopalakrishnan <abhi@login04.frontier.olcf.ornl.gov>
2025-11-12 13:23:26 -0500

    Generalized to grep U_g

--- postprocessing_scripts/grep_logTime.py
+++ postprocessing_scripts/grep_logTime.py
@@ -3,8 +3,9 @@ import numpy as np
 import csv                                                                      
 import os                                                                       
 import sys
-                                                                                
-                                                                                
+import argparse
+
+
 def grep_logTime(filename, st):                                                 
     text = pathlib.Path(filename).read_text()                                   
     pat = re.compile(rf"\b{re.escape(st)}\s*:\s*([+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?)")
@@ -14,7 +15,7 @@ def grep_logTime(filename, st):
     return n                                                                    
                                                                                 
 
-def grep_ctr(st, ctr_file="incompressible_tml.ctr"):                            
+def grep_from_file(st, file="incompressible_tml.ctr"):                            
     """ To grep all the required data from the CTR file                         
                                                                                 
     Args:                                                                       
@@ -24,7 +25,7 @@ def grep_ctr(st, ctr_file="incompressible_tml.ctr"):
         The variable's value                                                    
     """                                                                         
                                                                                 
-    text = pathlib.Path(ctr_file).read_text()                                   
+    text = pathlib.Path(file).read_text()                                   
     pat = re.compile(rf"\b{re.escape(st)}\s*=\s*([+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?)")
     m = pat.search(text)                                                        
     n   = float(m.group(1)) if m else None                                      
@@ -32,12 +33,12 @@ def grep_ctr(st, ctr_file="incompressible_tml.ctr"):
     return n
                                                                                 
 
-def single_log(shift_step=0):
-    nx_g        = int(grep_ctr("nx"))
-    output_step = int(grep_ctr("output_step"))
-    dt          = grep_ctr("dt")                                                       
+def single_log(Init_fname, shift_step=0):
+    nx_g        = int(grep_from_file("nx"))
+    output_step = int(grep_from_file("output_step"))
+    dt          = grep_from_file("dt")                                                       
     delta       = (2 * np.pi) / 100                                             
-    U_g         = 3.1830988618379066                                            
+    U_g         = grep_from_file("U1", f"./job_outputs/{Init_fname}")
 
     first_time_step_number  = 0
     final_time_step_file    = max(pathlib.Path(".").glob("time_step*"), key=lambda p: p.stat().st_mtime, default=None)
@@ -58,19 +59,19 @@ def single_log(shift_step=0):
         os.fsync(f.fileno())                                                    
     
 
-def multiple_log(shift_step=0):                                                                     
+def multiple_log(Init_fname, shift_step=0):                                                                     
     log  = sorted(pathlib.Path("./log").glob("log_*"))                               
-    nx_g = grep_ctr("nx_g")
+    nx_g = grep_from_file("nx_g")
                                                                                 
     f = open(f"Time_n{nx_g}.csv", "w", newline="")                                 
     w = csv.writer(f)                                                           
     w.writerow(["Time_step", "Time", "TimeNormalized"])                         
                                                                                 
-    output_step = grep_ctr("output_step")                                                          
-    dt          = grep_ctr("dt")                                                       
+    output_step = grep_from_file("output_step")                                                          
+    dt          = grep_from_file("dt")                                                       
     delta       = (2 * np.pi) / 100                                             
-    U_g         = 3.1830988618379066                                            
-                                                                                
+    U_g         = grep_from_file("U1", f"./job_outputs/{Init_fname}")
+
     for i, filename in enumerate(log):                                          
         time_step    = i * output_step + shift_step                             
         time         = (grep_logTime(filename, 'Time'))                         #time = dt * time_step; Well not technical    ly, since of adapt time
@@ -84,10 +85,14 @@ def multiple_log(shift_step=0):
 
 if __name__ == "__main__":                                                      
     #If multiple log files
-    if len(sys.argv) > 1:
+    #number_of_log = parse_args(1)
+    Init_fname = input("Enter initialiation job file: ")
+
+    number_of_log = input("Grepping from multiple log files? ")
+    if(number_of_log == "Yes"):
         #shiftstep=0
-        multiple_log()
+        multiple_log(Init_fname)
     
     else:
         #shiftstep=0
-        single_log()
+        single_log(Init_fname)
