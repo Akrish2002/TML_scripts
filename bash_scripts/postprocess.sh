2d595bdcf3281532a5cdb666398db2489a968e10
Abhijeet Gopalakrishnan <abhi@login04.frontier.olcf.ornl.gov>
2025-11-12 14:31:23 -0500

    Conditionals implemented

--- bash_scripts/postprocess.sh
+++ bash_scripts/postprocess.sh
@@ -8,15 +8,26 @@ set -e
 #- Redirecting postprocessed data to processed data folder
 #- asdf
 
-echo "--Submitting batch job to postprocess data..."
+echo "--Functions:
+        [1] Growth Rate
+        [2] Compute times from log file(s)
+        [3] asdf
+     "
+read -r arg
 
-#Script to compute growth rate
-echo "--Computing growth rate.."
-sbatch compute_growthrate.sh
+if [[ $arg == "1" ]]; then
+    #Script to compute growth rate
+    echo "--Submitting batch job to compute growth rate.."
+    sbatch compute_growthrate.sh
 
-#Script to compute time
-echo "--Computing normalized time.."
-compute_normalizedtime.sh
+elif [[ $arg == "2" ]]; then
+    #Script to compute time
+    echo "--Computing normalized time.."
+    compute_normalizedtime.sh
 
+else 
+    echo"--Nothing entered!"
+
+fi
 
 
