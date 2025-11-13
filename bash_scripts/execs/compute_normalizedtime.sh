52b6c48ecd4035966dc3ff99c1baf0077c58c40b
Abhijeet Gopalakrishnan <abhi@login05.frontier.olcf.ornl.gov>
2025-11-13 16:32:22 -0500

    Data move changes

--- bash_scripts/execs/compute_normalizedtime.sh
+++ bash_scripts/execs/compute_normalizedtime.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 #File locations
 FPCSLPY="/ccs/home/abhi/source_code/envs/fpcsl"
 PYTHONSCRIPT="/ccs/home/abhi/member-work/incompressible-tml/TML_scripts/postprocessing_scripts"
+echo "--Enter location to dump data: "
+read -r PATH_TO_DATA
 
 #Load modules
 module load miniforge3
@@ -12,5 +14,8 @@ source activate $FPCSLPY
 
 #Running the script on the login node, its fine!
 python3 $PYTHONSCRIPT/grep_logTime.py
+
+#Copying the data to plot/data for plotting
+cp *.csv $PATH_TO_DATA
 
 mv *.csv ./postprocessed_data
