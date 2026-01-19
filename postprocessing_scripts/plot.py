a6844514ef2fa1c4ff13467dae2ec004dc40d07a
Abhijeet Gopalakrishnan <abhi@login08.frontier.olcf.ornl.gov>
2026-01-19 14:24:06 -0500

    Updates to plot.py

--- postprocessing_scripts/plot.py
+++ postprocessing_scripts/plot.py
@@ -1,11 +1,13 @@
 import numpy as np                                                              
 import matplotlib.pyplot as plt                                                 
+import os
                                                                                 
 
+n128_data = "integrand_data_n128.csv"
 n256_data = "integrand_data_n256.csv"
 n512_data = "integrand_data_n512.csv"
 n1024_data = "integrand_data_n1024.csv"
-files = [["n256", "n512", "n1024"], [n256_data, n512_data, n1024_data]]
+files = [["n128", "n256", "n512", "n1024"], [n128_data, n256_data, n512_data, n1024_data]]
 integrands = [["Time"], ["Momentum_Thickness", "VolFrac_Thickness", "Mixing_Thickness"]]
 
 
@@ -23,10 +25,16 @@ def perform_plotting(specific_dataset = False):
             stop  = specific_dataset
         else:
             start = 0
-            stop  = 3 
+            stop  = 4 
         for jj in range(start, stop):
-            plot_thickness(files[1][jj], 1, label=files[0][jj], linestyle="-")  
+            if not os.path.exists(files[1][jj]):
+                print(f"--File {files[1][jj]} not found, skipping.")
+                continue
 
+            #if jj == 0:
+            #    continue
+
+            plot_thickness(files[1][jj], 1, label=files[0][jj], linestyle="-")  
             plt.xlabel(integrands[0][0])                                                              
             plt.ylabel(integrands[1][ii])                                                
             plt.legend()                                                                    
@@ -36,11 +44,12 @@ def perform_plotting(specific_dataset = False):
 
 
 def main(): 
-    arg = input("To plot all? ")
+    arg = input("--To plot all? ")
     if(arg == "Yes"):
         perform_plotting()
     else:
-        perform_plotting(1)
+        to_plot = input("--Which dataset do you wish to plot? ")
+        perform_plotting(to_plot)
 
 
 if __name__ == "__main__":
