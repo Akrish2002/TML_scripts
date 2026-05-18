1ec8179c0c26339d6c5da841d58cd6f53300548b
Abhijeet Gopalakrishnan <abhi@login04.frontier.olcf.ornl.gov>
2026-05-18 14:23:24 -0400

    Tried cleaning it up, use v2 instead

--- postprocessing_scripts/plot_growth_rates_v1.py
+++ postprocessing_scripts/plot_growth_rates_v1.py
@@ -1,15 +1,29 @@
 import numpy as np                                                              
 import matplotlib.pyplot as plt                                                 
 import os
+import argparse
                                                                                 
+datasetparser = argparse.ArgumentParser(description="Read optional CSV datasets")
 
-n128_data = "integrand_data_n128.csv"
-n256_data = "integrand_data_n256.csv"
-n512_data = "integrand_data_n512.csv"
-n1024_data = "growth_rate_data_n1024.csv"
-files = [["n128", "n256", "n512", "n1024"], [n128_data, n256_data, n512_data, n1024_data]]
+datasetparser.add_argument("--dataset_1", type=str, default=None, help="CSV file for dataset 1")
+datasetparser.add_argument("--dataset_2", type=str, default=None, help="CSV file for dataset 2")
+datasetparser.add_argument("--dataset_3", type=str, default=None, help="CSV file for dataset 3")
+datasetparser.add_argument("--dataset_4", type=str, default=None, help="CSV file for dataset 4")
+datasetparser.add_argument("--dataset_5", type=str, default=None, help="CSV file for dataset 5")
+datasetparser.add_argument("--dataset_6", type=str, default=None, help="CSV file for dataset 6")
+
+datasets = datasetparser.parse_args()
+dataset_1 = datasets.dataset_1
+dataset_2 = datasets.dataset_2
+dataset_3 = datasets.dataset_3
+dataset_4 = datasets.dataset_4
+dataset_5 = datasets.dataset_5
+dataset_6 = datasets.dataset_6
+files = [
+    ["dataset_1", "dataset_2", "dataset_3", "dataset_4", "dataset_5", "dataset_6"],
+   [dataset_1, dataset_2, dataset_3, dataset_4, dataset_5, dataset_6],
+]
 integrands = [["Time"], ["Momentum_Thickness", "VolFrac_Thickness", "Mixing_Thickness"]]
-
 
 def plot_thickness(filename, a, label, linestyle="-"):                          
     data = np.genfromtxt(filename, delimiter=",", skip_header=1)                
@@ -27,12 +41,12 @@ def perform_plotting(specific_dataset = False):
             start = 0
             stop  = 4 
         for jj in range(start, stop):
+            if files[1][jj] is None:
+                continue
+
             if not os.path.exists(files[1][jj]):
                 print(f"--File {files[1][jj]} not found, skipping.")
                 continue
-
-            #if jj == 0:
-            #    continue
 
             plot_thickness(files[1][jj], 1, label=files[0][jj], linestyle="-")  
             plt.xlabel(integrands[0][0])                                                              
@@ -45,6 +59,7 @@ def perform_plotting(specific_dataset = False):
 
 def main(): 
     arg = input("--To plot all? ")
+
     if(arg == "Yes"):
         perform_plotting()
     else:
