68ccea1c1dc76c543ee5d04564936d09c2561f60
Abhijeet Gopalakrishnan <agopalak@aurora-uan-0011.hostmgmt.cm.aurora.alcf.anl.gov>
2026-05-28 18:56:04 +0000

    Update

--- postprocessing_scripts/pyscripts/spectra.py
+++ postprocessing_scripts/pyscripts/spectra.py
@@ -1,11 +1,30 @@
 import numpy as np
 from mpi4py import MPI
 from pathlib import Path
-
 import matplotlib as mpl                                                        
 import matplotlib.pyplot as plt
+import re, pathlib
+import os
 
 from pyscripts.test_TKE_vGPT_v3 import TKE_Budget
+
+def grep_ctr(st, ctr_file="incompressible_tml.ctr"):
+    """ To grep all the required data from the CTR file
+    
+    Args:
+        st (string) : The variable whose data is to be grep-ed
+    
+    Return:
+        The variable's value
+    """
+    
+    text = pathlib.Path(ctr_file).read_text()
+    pat = re.compile(rf"\b{re.escape(st)}\s*=\s*([+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?)")
+    m = pat.search(text)
+    n   = float(m.group(1)) if m else None
+
+    return n
+
 
 def grep_timestep(path = "."):                                                  
     """ Grepping time steps to calculate first step, step and last step         
@@ -40,16 +59,20 @@ def compute_spectra(args):
     if T._case.rank == 0:
         #Grepping the required data 
         E_kx = T._E_kx_global
-
         kx   = T._kx_positive_global
 
         ny   = T._ny_g
-
-        #y_max = T._b * np.pi
         y_max = T._ymax
-
-        #Generate y at the cell centers, hardcoded for 2pi
         y = (np.arange(ny) + 0.5) * (y_max / ny)  
+
+        #Computing normalized time
+        U_l             = 0.
+        U_g             = 3.1830988618379066
+        ctr_file        = os.path.join(args.case, "incompressible_tml.ctr")
+        dt              = grep_ctr('dt', ctr_file)
+        delta_ts        = (2. * np.pi) / 100.
+        ts              = args.time_step
+        t_normalized    = (ts * dt * U_g)/delta_ts
 
         #if E_kx.ndim != 1 or E_kx.shape[0] != ny:
         if E_kx.shape[0] != ny:
@@ -58,18 +81,19 @@ def compute_spectra(args):
         out_path = Path(args.output_path)
         out_path.parent.mkdir(parents=True, exist_ok=True)
     
-        #out_path = out_dir / f"{nx}_{int(args.ts)}.npz"
         out_path = f"{ny}_{int(args.time_step)}.npz"
 
         np.savez(
                     out_path,
-                    case=str(Path(args.case).resolve()),
-                    time_step=int(args.time_step),
-                    ny=int(ny),
-                    #Value stored, custom for each script
-                    y=y.astype(np.float64),
-                    E_kx=E_kx.astype(np.complex128),
-                    kx=kx.astype(np.float64)
+                    case            =   str(Path(args.case).resolve()),
+
+                    time_step       =   int(args.time_step),
+                    t_normalized    =   np.float64(t_normalized),
+                    ny              =   int(ny),
+
+                    y               =   y.astype(np.float64),
+                    kx              =   kx.astype(np.float64),
+                    E_kx            =   E_kx.astype(np.complex128)
                     
                 )
         print(f"[rank0] wrote {out_path} (ny={ny}, ts={args.time_step})")
@@ -90,7 +114,11 @@ def apply_paper_style(ax):
 def load_npz_spectra(path: str):
     d    = np.load(path, allow_pickle=True)
     case = str(d["case"])
-    y    = d["y"].astype(np.float64)
+
+    t_normalized    = float(d["t_normalized"])
+    y               = d["y"].astype(np.float64)
+    ny              = int(d["ny"])
+
     kx   = d["kx"].astype(np.float64)
     E_kx = d["E_kx"].astype(np.complex128)
 
@@ -103,8 +131,8 @@ def load_npz_spectra(path: str):
     if E_kx.ndim == 1 or E_kx.shape[1] != kx.shape[0]:
         raise ValueError(f"Size error, please check the generated dataset!")
 
-    #Returning spectra only at the centerplane
-    return case, kx, E_kx[len(E_kx) // 2]
+    print("-- Returning spectra only at the centerplane!")
+    return case, t_normalized, ny, kx, E_kx[len(E_kx) // 2]
 
 def plot_spectra(args):
     #Loads only the E_kx along the centerline
@@ -116,16 +144,19 @@ def plot_spectra(args):
     ax = fig.add_subplot(111)                                                   
     dash_cycle = ["-", ":", "--", "-.", (0, (5, 2)), (0, (3, 1, 1, 1))]
 
-    for idx, (case, kx, E_kx) in enumerate(entries):
+    for idx, (case, t_normalized, ny, kx, E_kx) in enumerate(entries):
         lab = (
                 args.labels[idx]
                 if args.labels and len(args.labels) == len(entries)
-                else f"{Path(case).name}"
+                else f"{ny}$^3$, t*={t_normalized:.2f}"
               )
 
         #Zoom mask in this
+        ax.legend(fontsize=8)
         if args.zoom:
             print("--Zoom has not been implemented for spectra plotting")
+            ax.loglog(kx[1:], E_kx[1:], color="r", 
+                    linestyle=dash_cycle[idx % len(dash_cycle)], linewidth=1.2, label=lab)
 
         else:
             ax.loglog(kx[1:], E_kx[1:], color="r", 
@@ -145,8 +176,8 @@ def plot_spectra(args):
 
 
     #Labels
-    ax.set_ylabel("E_kx")
-    ax.set_xlabel("kx")
+    ax.set_ylabel("$\E_uu$")
+    ax.set_xlabel("$\kx$")
     #To have path of run being used
     p = Path(case)
     short = Path(*p.parts[-2:])
@@ -158,7 +189,7 @@ def plot_spectra(args):
     )
 
     if args.zoom:
-        print("--Zoom has not been implemented for spectra plotting")
+        print("--Zoom has not been implemented for spectra plotting!")
 
     apply_paper_style(ax)
     ax.legend(loc="best", frameon=False)
